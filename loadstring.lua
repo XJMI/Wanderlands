d2a2560aef5dbf01066be6f1674118a6cbe9ea15
XJMI <59071906+XJMI@users.noreply.github.com>
2024-11-24 21:27:06 -0500

    Create loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,15 @@
+--[[
+   __      __   .__                
+  /  \    /  \  |  |  __ _______   
+  \   \/\/   /  |  | |  |  \__  \  
+   \        /   |  |_|  |  // __ \_
+    \__/\  / /\ |____/____/(____  /
+         \/  \/                 \/ 
+
+  - Thank you for using this script.
+  - This script will forever be 100% Undetected.
+  - Known supported executors: SynZ feel free to test it tho.
+
+]]--
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/XJMI/Wanderlands/refs/heads/main/source.lua'))()
